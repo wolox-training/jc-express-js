1d6640c80882f8a165f210a0a6ca52fbd9048f56
Juan Castillo <juancamilo.castillo@wolox.co>
2021-02-09 08:57:27 -0500

    chage mock according to change wolox domain validator

--- test/mocks/users.mock.js
+++ test/mocks/users.mock.js
@@ -4,20 +4,20 @@ const { WOLOX_DOMAIN } = require('../../app/helpers');
 exports.signupGoodCase = {
   firstName: chance.first(),
   lastName: chance.last(),
-  email: chance.email({ domain: WOLOX_DOMAIN }),
+  email: chance.email({ domain: WOLOX_DOMAIN }) + chance.word({ length: 2 }),
   password: chance.word({ length: 8 }) + chance.integer({ min: 0, max: 9 })
 };
 
 exports.passwordWrongLen = {
   firstName: chance.first(),
   lastName: chance.last(),
-  email: chance.email({ domain: WOLOX_DOMAIN }),
+  email: chance.email({ domain: WOLOX_DOMAIN }) + chance.word({ length: 2 }),
   password: chance.word({ length: 5 }) + chance.integer({ min: 0, max: 9 })
 };
 
 exports.passwordWrongAlphanumeric = {
   firstName: chance.first(),
   lastName: chance.last(),
-  email: chance.email({ domain: WOLOX_DOMAIN }),
+  email: chance.email({ domain: WOLOX_DOMAIN }) + chance.word({ length: 2 }),
   password: chance.word({ length: 8 }) + chance.integer(`${{ min: 0, max: 9 }}_-`)
 };
